04b4661b0c0ceaadd732a000c6ca29c76e701f2c
n3m3sis42 <n3m3sis42@gmail.com>
2018-05-04 13:16:58 -0400

    set up Google oauth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-router.get('/google', (req, res) => {
-  res.send('AUTHHHHH');
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/callback',
+  passport.authenticate('google',
+  { failureRedirect: '/' }), (req, res) => {
+  res.redirect('/dashboard');
 });
 
 module.exports = router;
