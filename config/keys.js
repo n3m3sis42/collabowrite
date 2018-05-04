1e834782cdd25629b5815ff60cdb19dcd7ce44a8
Sarah Evans <n3m3sis42@gmail.com>
2018-05-04 10:33:02 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://sarah:blah@ds113358.mlab.com:13358/db-collabowrite-dev',
-  googleClientID: '282089265613-hh9ogvbdj7e9gimah2icjaibbqr5j5gk.apps.googleusercontent.com',
-  googleClientSecret: 'DfZl3uRf8cNx4MnFvOvJ4dAd'
-};
