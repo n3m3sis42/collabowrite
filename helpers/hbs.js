a2f3b3adf97900293bd4bb1f59a51ca8f091bf32
n3m3sis42 <n3m3sis42@gmail.com>
2018-05-06 22:05:36 -0400

    hide edit buttons for unless story belongs to logged in user

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -15,5 +15,22 @@ module.exports = {
     return options.fn(this).replace(
       new RegExp(' value=\"' + selected + '\"'),
       '$& selected="selected"');
+  },
+  editIcon: function(storyUser, loggedInUser, storyId, floating = true) {
+    if (storyUser == loggedInUser) {
+      if (floating) {
+        return (
+          `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab red">
+            <i class="fa fa-pencil"></i>
+          </a>`
+        );
+      } else {
+        return (
+          `<a href="/stories/edit/${storyId}"><i class="fa fa-pencil"></i></a>`
+        );
+      }
+    } else {
+      return '';
+    }
   }
 };
